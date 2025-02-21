222107a585e2d593f672e054589a873477077ee4
Arman <armanarakelyandev@gmail.com>
2025-02-21 19:20:32 +0400

    users and user_locations added with a seeder

--- app/Actions/CheckWeatherAction.php
+++ app/Actions/CheckWeatherAction.php
@@ -2,7 +2,18 @@
 
 namespace App\Actions;
 
+use App\Services\WeatherAPI\WeatherAPIContract;
+
 class CheckWeatherAction
 {
+    public function __construct(
+        private WeatherAPIContract $weatherAPI
+    ) {
+    }
 
+    public function __invoke()
+    {
+        //TODO more weather APIs can be used in the future
+        $this->weatherAPI->getWeather();
+    }
 }
--- app/Services/WeatherAPI/WeatherBit.php
+++ app/Services/WeatherAPI/WeatherBit.php
@@ -4,7 +4,7 @@ namespace App\Services\WeatherAPI;
 
 use Illuminate\Support\Facades\Http;
 
-class WeatherBit
+class WeatherBit implements WeatherAPIContract
 {
     protected $result;
 
